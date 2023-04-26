cd408e3229a5304c8d2974da5903ca951fe5bee4
Binoo <binooprog@gmail.com>
2023-04-26 22:12:44 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    static Stage loginStage=new Stage();
+    @FXML
+    private Label error;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void Login(ActionEvent event) {
+        System.out.println("Loged");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root= FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+        Scene scene=new Scene(root);
+        loginStage.setScene(scene);
+        loginStage.setTitle("Hospital management");
+        loginStage.setResizable(false);
+        loginStage.show();
+    }
+}
